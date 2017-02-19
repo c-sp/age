ac179e80fa168e80d0b6f469738afb13231cf8f9
Christoph Sprenger <em73514@mailbox.org>
2017-02-19 11:30:22 +0100

    added script for running mooneye tests

---
+++ test/run_mooneye_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script expects:
+#   - the environment variable GB_REPO_MOONEYE_GB to contain the path to the mooneye-gb repository.
+#   - the age_emulator_test executable to be available.
+
+SOURCE_DIR=`dirname $0`
+./age_emulator_test --type mooneye --ignore-list $SOURCE_DIR/tests_to_ignore.txt $GB_REPO_MOONEYE_GB/tests/build
+
