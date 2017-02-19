a8e12c31b9270f44b8192d003264af55008ad07d
Christoph Sprenger <em73514@mailbox.org>
2017-02-19 12:03:59 +0100

    adjusted age_emulator_test path in mooneye test script

--- test/run_mooneye_tests.sh
+++ test/run_mooneye_tests.sh
@@ -5,5 +5,5 @@
 #   - the age_emulator_test executable to be available.
 
 SOURCE_DIR=`dirname $0`
-./age_emulator_test --type mooneye --ignore-list $SOURCE_DIR/tests_to_ignore.txt $GB_REPO_MOONEYE_GB/tests/build
+$SOURCE_DIR/age_emulator_test --type mooneye --ignore-list $SOURCE_DIR/tests_to_ignore.txt $GB_REPO_MOONEYE_GB/tests/build
 
