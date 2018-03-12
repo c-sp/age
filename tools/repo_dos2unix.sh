a5aef8ba1757aedf5480bfe9af3976c7c5932661
Christoph Sprenger <em73514@mailbox.org>
2018-03-12 19:29:43 +0100

    adjust dos2unix script: fix comment, include more files types

---
+++ tools/repo_dos2unix.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+SOURCE_DIR=`dirname $0`
+
+# Convert Windows line endings (CRLF) to Unix line endings (LF).
+# (for each file execute the "dos2unix" command)
+find $SOURCE_DIR/.. -regex ".*\.hpp\|.*\.cpp\|.*\.pro\|.*\.pri\|.*\.txt|.*\.yml|.*\.md" -type f -exec dos2unix {} \;
