597b53dcaf2ff98d54370967edc71d3adc84f63e
Christoph Sprenger <em73514@mailbox.org>
2018-04-02 12:13:38 +0200

    fix dos2unix script (the regex did not catch all files)

--- tools/repo_dos2unix.sh
+++ tools/repo_dos2unix.sh
@@ -4,4 +4,4 @@ SOURCE_DIR=`dirname $0`
 
 # Convert Windows line endings (CRLF) to Unix line endings (LF).
 # (for each file execute the "dos2unix" command)
-find $SOURCE_DIR/.. -regex ".*\.hpp\|.*\.cpp\|.*\.pro\|.*\.pri\|.*\.txt|.*\.yml|.*\.md|.*\.adoc|.*\.sh" -type f -exec dos2unix {} \;
+find $SOURCE_DIR/.. -regex ".*\.hpp\|.*\.cpp\|.*\.pro\|.*\.pri\|.*\.txt\|.*\.yml\|.*\.md\|.*\.adoc\|.*\.sh" -type f -exec dos2unix {} \;
