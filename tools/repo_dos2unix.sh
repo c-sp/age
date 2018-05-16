fc6d04bcfec5d992e9ae66fe1bf0e1f4f383143a
Christoph Sprenger <em73514@mailbox.org>
2018-05-16 17:51:43 +0200

    remove unused script

--- tools/repo_dos2unix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-SOURCE_DIR=`dirname $0`
-
-# Convert Windows line endings (CRLF) to Unix line endings (LF).
-# (for each file execute the "dos2unix" command)
-find $SOURCE_DIR/.. -regex ".*\.hpp\|.*\.cpp\|.*\.pro\|.*\.pri\|.*\.txt\|.*\.yml\|.*\.md\|.*\.adoc\|.*\.sh" -type f -exec dos2unix {} \;
